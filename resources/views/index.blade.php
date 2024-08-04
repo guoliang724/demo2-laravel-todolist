01f7ed81ba27c43f6bebf66d864fa50aa94b7fed
Leon Zhang <leonzhang.developer@gmail.com>
2024-08-04 16:59:46 -0600

    feat: layout using template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.app')
+
+@section('title', 'Hello I am a blade template')
+
+@section('content')
+@if (count($tasks))
+    @foreach ($tasks as $task) 
+        <a href="{{route('tasks.show', ['id' => $task->id])}}">{{$task->title}}</a>
+        <br>
+    @endforeach
+@else 
+    <div>There are no tasks!</div>
+@endif
+
+@endsection
