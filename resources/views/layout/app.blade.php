40bffceb6ab2943acaa56bfd03883f0f17802b3e
Leon Zhang <leonzhang.developer@gmail.com>
2024-08-05 18:42:42 -0600

    feat: validation,session,flash message

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @yield("style")
 </head>
 
 <body>
     <H1>@yield("title")</H1>
     <div>
+        @if(session()->has('sucess'))
+            <div>{{session('sucess')}}</div>
+        @endif
         @yield("content")
     </div>
 </body>
